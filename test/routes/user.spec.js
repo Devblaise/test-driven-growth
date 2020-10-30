ab65168ed8b31a86a10501a84c0e16755a7a4621
Kenechukwu Mbadugha <blaisembadugha@gmail.com>
2020-10-30 19:56:15 +0100

    removed updated code from TDD

--- test/routes/user.spec.js
+++ test/routes/user.spec.js
@@ -97,23 +97,6 @@ describe("User test suite", () => {
       });
   });
 
-  // code added  post to update data
-  it("should PUT/users/:id", () => {
-    // data to update
-    const data = {
-      status: "Active",
-      name: `Luffy - ${Math.floor(Math.floor() * 9999)}`,
-    };
-
-    return request
-      .put("users/132")
-      .set("Authorization", `Bearer ${TOKEN}`)
-      .send(data)
-      .then((res) => {
-        expect(res.body.data).to.deep.include(data);
-      });
-  });
-
   it("should DELETE /users/:id", () =>
     request
       .delete("users/2")
